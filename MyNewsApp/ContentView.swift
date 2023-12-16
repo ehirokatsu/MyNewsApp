3e276d03268fb70374cb2c3e438be667e685c060
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-12-16 20:12:15 +0900

    ニュースのタイトルをリストで表示

--- MyNewsApp/ContentView.swift
+++ MyNewsApp/ContentView.swift
@@ -1,23 +1,42 @@
-//
-//  ContentView.swift
-//  MyNewsApp
-//
-//  Created by 遠藤裕雄 on 2023/12/16.
-//
-
 import SwiftUI
+import Combine
+
+
+struct News: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let title: String
+}
 
 struct ContentView: View {
+    @State private var articles: [Article] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello!5")
+        List(articles, id: \.title) { article in
+            Text(article.title)
         }
-        .padding()
+        .onAppear(perform: loadNews)
+    }
+
+    func loadNews() {
+        let urlString = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=7b2095031fa64f83b6aaf34169dc870e"
+        guard let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                let decoder = JSONDecoder()
+                if let news = try? decoder.decode(News.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.articles = news.articles
+                    }
+                }
+            }
+        }.resume()
     }
 }
+
 
 #Preview {
     ContentView()
