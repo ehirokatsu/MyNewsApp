644147de1ecbf9c4006a3958b7eb2f66f9a92e8e
hiro <48009332+ehirokatsu@users.noreply.github.com>
2023-12-16 21:59:46 +0900

    ターゲットOSを10.1に変更。Watchにも同じNewsAPi取得処理を追加

--- MyNewsApp Watch App/ContentView.swift
+++ MyNewsApp Watch App/ContentView.swift
@@ -1,23 +1,54 @@
-//
-//  ContentView.swift
-//  MyNewsApp Watch App
-//
-//  Created by 遠藤裕雄 on 2023/12/16.
-//
-
 import SwiftUI
+import Combine
+
+
+struct News: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let title: String
+    let description: String? //contentはnullの場合もあるから
+}
 
 struct ContentView: View {
+    @State private var articles: [Article] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(articles, id: \.title) { article in
+                NavigationLink(destination: Text(article.description ?? "No content available")) {
+                    Text(article.title)
+                }
+            }
+            .onAppear(perform: loadNews)
         }
-        .padding()
+            /*
+        List(articles, id: \.title) { article in
+            Text(article.title)
+        }
+        .onAppear(perform: loadNews)
+        */
+            
+    }
+
+    func loadNews() {
+        let urlString = "https://newsapi.org/v2/top-headlines?country=jp&apiKey=7b2095031fa64f83b6aaf34169dc870e"
+        guard let url = URL(string: urlString) else { return }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                let decoder = JSONDecoder()
+                if let news = try? decoder.decode(News.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.articles = news.articles
+                    }
+                }
+            }
+        }.resume()
     }
 }
+
 
 #Preview {
     ContentView()
