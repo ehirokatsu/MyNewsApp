7cdd7d167e40c832e682b1065f8d1e6a529f6b49
ehirokatsu <48009332+ehirokatsu@users.noreply.github.com>
2023-12-16 14:20:23 +0900

    テストコミット3

--- MyNewsApp/ContentView.swift
+++ MyNewsApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello!2")
+            Text("Hello!3")
         }
         .padding()
     }
