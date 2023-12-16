b192c569079a982816969ba80da8b03a2677e1ca
hiro <e.hirokatsu@gmail.com>
2023-12-16 14:34:08 +0900

    テストコミットAir

--- MyNewsApp/ContentView.swift
+++ MyNewsApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello!3")
+            Text("Hello!4")
         }
         .padding()
     }
